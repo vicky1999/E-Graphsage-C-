59f8a9c8b4a0d9a052f37a240dc914e88d7d6915
Vignesh <ravivignesh2000@gmail.com>
2022-03-16 22:54:17 +0530

    schema added

--- src/EGraphSage.cpp
+++ src/EGraphSage.cpp
@@ -23,10 +23,11 @@ int main(int argc, char **argv)
     */
 
     string node_filename = "", ip_filename = "";
+    string node_schema = "", ip_schema = "";
     char buff[1000];
     char ch;
 
-    while ((ch = getopt(argc, argv, "i:n:")) != -1)
+    while ((ch = getopt(argc, argv, "i:n:v:e:")) != -1)
     {
         switch (ch)
         {
@@ -36,19 +37,25 @@ int main(int argc, char **argv)
         case 'n':
             ip_filename = optarg;
             break;
+        case 'v':
+            node_schema = optarg;
+            break;
+        case 'e':
+            ip_schema = optarg;
+            break;
         }
     }
 
-    if (node_filename == "" || ip_filename == "")
+    if (node_filename == "" || ip_filename == "" || node_schema == "" || ip_schema == "")
     {
-        print_error("Node file and ip file are required!");
+        print_error("Node file, ip file, node schema and ip schema are required!");
         return 0;
     }
 
     getcwd(buff, 1000);
     string path(buff);
     string basepath = path.substr(0, path.find_last_of("/\\"));
-    string command_string = "python3 " + basepath + "/python/featherparser.py -i " + node_filename + " -n " + ip_filename;
+    string command_string = "python3 " + basepath + "/python/featherparser.py -i " + ip_filename + " -n " + node_filename;
     const char *command = command_string.c_str();
     system(command);
 
